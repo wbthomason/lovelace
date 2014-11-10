cc11430be67083569ccc8e47a16923022d4e7e05
Wil Thomason <wil.thomason@gmail.com>
2014-11-10 01:00:12 -0500

    Fun formatting

--- lyrics.py
+++ lyrics.py
@@ -45,31 +45,42 @@ def check_rhythm(song):
 
 def generate_lyrics(corpus, length):
     random.seed()
-    song = []
     curr = random.choice(list(corpus.keys()))
-    for i in range(length):
-        song.append(curr)
-        follows = random.choice(corpus[curr])
-        size = random.randint(1, min(ngram_length, len(follows)))
-        curr = " ".join(follows[size - 1])
-    return song
+    def make_verse(seed):
+        while True:
+            song = []
+            for i in range(length):
+                song.append(seed)
+                follows = random.choice(corpus[seed])
+                size = random.randint(1, min(ngram_length, len(follows)))
+                seed = " ".join(follows[size - 1])
+            yield song
+    return make_verse(curr)
 
 def make_song(artist_name):
     raw_words = get_lyrics(artist_name)
     print("Got lyrics!")
     lyric_ngrams = make_ngrams(raw_words)
     print("Made ngrams!")
-    song_length = random.randint(50,500)
-    raw_song = " ".join(generate_lyrics(lyric_ngrams, song_length))
-    song = ""
-    for c in raw_song:
-        if c.isupper() and c != "I":
-            song += "\n"
-        song += c
-    return song
+    num_verses = random.randint(3, 6)   
+    verse_length = random.randint(50,150)
+    verses = []
+    verse_generator = generate_lyrics(lyric_ngrams, verse_length)
+    for i in range(num_verses):
+        verses.append(next(verse_generator))
+    chorus = next(generate_lyrics(lyric_ngrams, random.randint(20,50)))
+    title_start = random.randint(0, len(chorus)-1)
+    title_len = random.randint(1, 3)
+    title = " ".join(chorus[title_start:title_start + title_len] if title_start + title_len < len(chorus) else chorus[title_start:])
+    return [title, " ".join(chorus), verses]
 
 if __name__=="__main__":
     if len(sys.argv) < 2:
         print("Incorrect usage. Please provide the name of an artist to emulate")
     else:
-       print(make_song(" ".join(sys.argv[1:])))
+       artist = " ".join(sys.argv[1:])
+       song = make_song(artist)
+       print('"{}", by {}'.format(song[0], artist))
+       print("[Chorus]\n{}".format(song[1]))
+       for i in range(1, len(song[2]) + 1):
+           print("[Verse {}]\n{}".format(i, " ".join(song[2][i-1])))
